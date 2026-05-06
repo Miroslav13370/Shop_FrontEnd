1b99ce1d36e770bc4ff6dee8613528b9dabdb169
ministrfinansov <ministrfinansov@users.noreply.github.com>
2026-05-06 13:28:08 +0000

    update project

--- next.config.ts
+++ next.config.ts
@@ -2,19 +2,16 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
 	
-	env: {
-		SERVER
-	}
-	
-  images: {
-    remotePatterns: [
-      { protocol: 'https', hostname: 'lh3.googleusercontent.com' },
-      { protocol: 'https', hostname: 'avatars.yandex.net' },
-      { protocol: 'http', hostname: 'localhost' },
-    ],
-    contentDispositionType: 'inline',
-    dangerouslyAllowLocalIP: true,
-  },
+images: {
+  remotePatterns: [
+    { protocol: 'https', hostname: 'lh3.googleusercontent.com' },
+    { protocol: 'https', hostname: 'avatars.yandex.net' },
+    { protocol: 'http', hostname: 'localhost' },
+    { protocol: 'http', hostname: '136.234.124.13', pathname: '/uploads/**' },
+  ],
+  contentDispositionType: 'inline',
+  dangerouslyAllowLocalIP: true,
+},
 
   async rewrites() {
     return [
